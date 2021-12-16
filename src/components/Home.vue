c02c9d01cedf426c83c526066428e4a9b0df4788
Sarah Carter <secarter114@gmail.com>
2021-12-16 00:23:16 -0500

    overall layout set

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -15,29 +15,80 @@
         <button>Adopt Now</button>
       </div>
     </div>
-    <div class="middle-container">
-      <div id="volunteer-container">
-        <h2 id="volunteer-header">VOLUNTEERS</h2>
-        <img
-          id="volunteer-image"
-          src="../assets/img/girl.png"
-          alt="Girl carrying dog"
-        />
-        <p>
-          Amazing people, doing the difference evrey single day. Join us know
-          and be a part of a community that is changing the world
-        </p>
-        <button>Join Us</button>
-      </div>
-      <div id="testimonial-container">
-        <h2 id="testimonial-header">TESTIMONIAL OF THE MONTH</h2>
-        <img
-          id="testimonial-image"
-          src="../assets/img/testimonial.jpg"
-          alt="testimonial video"
-        />
-      </div>
+
+    <!-- MIDDLE IMAGES -->
+
+    <b-container class="middle-container">
+      <b-row>
+        <b-col id="volunteer-container" cols="4">
+          <h2 id="volunteer-header">VOLUNTEERS</h2>
+          <img
+            id="volunteer-image"
+            src="../assets/img/girl.png"
+            alt="Girl carrying dog"
+          />
+          <p>
+            Amazing people, doing the difference evrey single day. Join us know
+            and be a part of a community that is changing the world
+          </p>
+          <button>JOIN US</button>
+        </b-col>
+        <b-col id="testimonial-container" cols="8">
+          <h2 id="testimonial-header">TESTIMONIAL OF THE MONTH</h2>
+          <img
+            id="testimonial-image"
+            src="../assets/img/testimonial.jpg"
+            alt="testimonial video"
+          />
+        </b-col>
+      </b-row>
+    </b-container>
+    <!-- PROGRAMS -->
+
+    <div id="mid-links">
+      <h2>PROGRAMS</h2>
+      <h2>ASSOCIATED COMPANIES</h2>
     </div>
+    <b-container id="dog-vest-container">
+      <b-row>
+      <b-col>
+        <b-container class="programs">
+          <b-row class="programs-container">
+            <b-col cols="2"
+              ><img src="../assets/img/friend-in-trouble.png" alt="" />
+            </b-col>
+            <b-col class="programs-text">
+              <h4>BEHAVIOR PROGRAM</h4>
+              <p>
+                Evaluation of each dog using specialist who determine the best
+                training that will help your friend be a better family member
+              </p>
+              <button class="read-more">READ MORE</button>
+            </b-col>
+          </b-row>
+          <b-row class="programs-container">
+            <b-col cols="2"
+              ><img src="../assets/img/dog-friends.png" alt="Two friendly dogs"
+            /></b-col>
+            <b-col class="programs-text">
+              <h4>MY NEXT FAMILY</h4>
+              <p>
+                Are you looking for a new family member? The this program is for
+                you. Don't buy, adopt here, and let's give a home to these
+                amazing friends
+              </p>
+              <button class="read-more">READ MORE</button>
+            </b-col>
+          </b-row>
+        </b-container>
+      </b-col>
+      <b-col cols="4"
+        ><img id="dog-in-vest" src="../assets/img/dog-in-vest.png" alt=""
+      /></b-col>
+      </b-row>
+    </b-container>
+
+    <!-- FOOTER -->
     <footer>
       <div class="logo-with-text">
         <img src="../assets/img/logo.png" alt="AdoptMe.org Logo" />
@@ -71,7 +122,6 @@ nav {
   color: white;
 }
 
-
 a {
   color: white;
   text-decoration: none;
@@ -91,30 +141,32 @@ a {
 
 .hero-text {
   background-color: black;
-  opacity: .3;
+  opacity: 0.3;
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  
+  width: 100%;
+  vertical-align: text-bottom;
+  color: $color-red;
 }
 
-.hero-text h1 {
-  color: $color-blue;
-  opacity: 100%;
+.hero-text button {
+  border: 2px $color-red solid;
+  background-color: transparent;
+  padding: 0 1%;
+  font-weight: bold;
+  margin-left: 5%;
+  color: $color-red;
 }
 
 .middle-container {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-template-areas: "volunteer testimonial";
-  grid-gap: 4%;
   margin: 5% 8%;
 }
 
-.volunteer-container {
-  grid-area: volunteer;
-  border: 1px solid black;
+#volunteer-container {
+  box-shadow: 2px 3px 10px rgb(0 0 0 / 0.2);
+  padding: 0;
 }
 
 #volunteer-header {
@@ -122,6 +174,22 @@ a {
   background-color: $color-blue;
   padding: 0.5rem;
   margin: 0px auto;
+}
+
+#volunteer-container button {
+  border: 2px $color-blue solid;
+  background-color: white;
+  padding: 1% 5%;
+  font-weight: bold;
+  color: $color-blue;
+  margin-left: 5%;
+}
+
+#volunteer-container p {
+  color: $color-gray;
+  padding: 5% 27%;
+  padding-left: 5%;
+  font-size: x-large;
 }
 
 #volunteer-image {
@@ -138,7 +206,63 @@ a {
   margin: 0px auto;
 }
 
+#testimonial-container img {
+  width: 100%;
+}
+
+#mid-links {
+  display: flex;
+  flex-direction: row;
+  font-weight: bolder;
+  margin-left: 6%;
+}
+
+#mid-links h2 {
+  margin-right: 5%;
+}
+
+#mid-links h2:hover {
+  color: $color-blue;
+  text-decoration: underline;
+}
+
+// .programs-container {
+//   margin-left: 10%;
+//   margin-bottom: 2%;
+// }
+
+.programs-container img {
+  width: 100%;
+}
+
+.programs-container h4 {
+  color: $color-red;
+  font-weight: bold;
+}
+.programs-container p {
+  color: $color-gray;
+}
+
+.programs-container button {
+  border: 2px $color-red solid;
+  background-color: white;
+  padding: 1% 5%;
+  font-weight: bold;
+  color: $color-red;
+}
+
+// .programs-container{
+//   display: flex;
+//   flex-direction: row;
+// }
+
+#dog-in-vest {
+  display: inline-block;
+  width: 100%;
+ vertical-align: middle;
+}
 footer {
   background-color: $color-blue;
+  display: flex;
 }
 </style>
